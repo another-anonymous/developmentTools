7a2042342e49e0011f91a94a70f44741ca4ce403
Anonymous <gempro2012@hotmail.com>
2013-05-17 21:05:14 +0300

    Anonymous commit

---
+++ DateTimeWork.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class DateTimeWork
+{
+   public void GetCurrentTime()
+   {
+		var current = DateTime.Parse("Friday, May 17, 2013");
+   }
+}
---
+++ Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleT4Template
+{
+    class Demo
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
